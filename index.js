1230ee2c939cb65dd9cba8ec1e3642586145369c
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-08 22:36:40 +0600

    jwt token is setting

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser=require('cookie-parser');
 const port =process.env.PORT || 5000;
 require('dotenv').config();
-//middleware
-app.use(cors());
+const jwt = require('jsonwebtoken');
+
+
+//middlewsonwebtokene
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('car doctor is running!');
@@ -14,13 +23,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
-//CarDoctor
-//aSFTxz4c7AG6FRXd
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pflyccd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -31,7 +37,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -61,6 +66,8 @@ async function run() {
         res.json(result);
     });
     app.get('/bookings', async (req, res) => {
+      console.log('cookies:',req.cookies);
+      console.log('token:',req.cookies.token);  
       let query={};
         let cursor= bookingsCollection.find(query);
         if(req.query.email){
@@ -77,7 +84,8 @@ async function run() {
       const result=await bookingsCollection.deleteOne(query);
       res.json(result);
     });
-    app.patch('/bookings/:id',async (req,res)=>{
+    app.patch
+    ('/bookings/:id',async (req,res)=>{
       const id=req.params.id;
       const query={_id: new ObjectId(id)};
       const updatedBooking=req.body;
@@ -87,6 +95,25 @@ async function run() {
       };
       const result=await bookingsCollection.updateOne(query,updateDoc);
       res.json(result);
+    });
+
+    //auth related
+
+    app.post('/jwt',async (req,res)=>{
+      const user=req.body;
+      console.log(user);
+      const token=jwt.sign(user,process.env.Access_Token_Secret,{
+        expiresIn:'1h'
+      
+      });
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:'none'
+      
+      })
+      .send({success:true});
     });
   } finally {
     // Ensures that the client will close when you finish/error
